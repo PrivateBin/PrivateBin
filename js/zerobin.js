7cb345001aabf4456e862c3147fd688cd9316425
Frédérik Paradis <fredy_14@live.fr>
2012-05-10 14:35:33 -0400

    Change URL on clone

--- js/zerobin.js
+++ js/zerobin.js
@@ -328,6 +328,10 @@ function stateExistingPaste() {
  */
 function clonePaste() {
     stateNewPaste();
+    
+    //Erase the id and the key in url
+    history.replaceState(document.title, document.title, scriptLocation());
+    
     showStatus('');
     $('textarea#message').text($('div#cleartext').text());
 }
