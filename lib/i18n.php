4918bef4dc8a6779c6deb9084e92d24c18de0475
El RIDO <elrido@gmx.net>
2016-04-26 20:21:30 +0200

    Although there usually are no plurals in chinese, there's an exception for words related to persons, when not preceeded by a numeric word.
    Sources:
    - http://localization-guide.readthedocs.org/en/latest/l10n/pluralforms.html#f3
    - https://answers.yahoo.com/question/index?qid=20110606153553AAAW5zX

--- lib/i18n.php
+++ lib/i18n.php
@@ -303,6 +303,7 @@ class i18n
     {
         switch (self::$_language) {
             case 'fr':
+            case 'zh':
                 return ($n > 1 ? 1 : 0);
             case 'pl':
                 return ($n == 1 ? 0 : $n%10 >= 2 && $n %10 <=4 && ($n%100 < 10 || $n%100 >= 20) ? 1 : 2);
