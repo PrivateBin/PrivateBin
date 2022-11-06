0288d94a68c23b940a057f60b073017aa45babb8
El RIDO <elrido@gmx.net>
2022-11-06 09:27:51 +0100

    regenerate composer.lock with PHP 7.2 for Scrutinizer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,9 +13,6 @@ class ComposerAutoloaderInitDontChange
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
