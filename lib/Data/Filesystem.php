PrivateBin/PrivateBin

commit 3668f1e3f4808d7232606349035c6a43c5a84c23

attempt to accomodate Crowdin by providing a single source translation file that is not actually used or loaded by our code

lib/I18n.php
@@ -199,7 +199,6 @@ class I18n
                     self::$_availableLanguages[] = $match[1];
                 }
             }
-            self::$_availableLanguages[] = 'en';
         }
         return self::$_availableLanguages;
     }

commit a7f720d82545bacf062339eeb930581f678a6ad6

ensure English is always added to available languages, fixes #1208
bug got introduced in 3668f1e3f4808d7232606349035c6a43c5a84c23 and
started affecting release after 896a49c8cf96ae9351796bd84050d2a0b249ddb3

lib/Data/Filesystem.php
@@ -13,6 +13,7 @@
 namespace PrivateBin\Data;
 
 use Exception;
+use GlobIterator;
 use PrivateBin\Json;
 
 /**
@@ -394,7 +395,7 @@ class Filesystem extends AbstractData
     public function getAllPastes()
     {
         $pastes = array();
-        foreach (new \GlobIterator($this->_path . self::PASTE_FILE_PATTERN) as $file) {
+        foreach (new GlobIterator($this->_path . self::PASTE_FILE_PATTERN) as $file) {
             if ($file->isFile()) {
                 $pastes[] = $file->getBasename('.php');
             }

lib/I18n.php
@@ -12,6 +12,9 @@
 
 namespace PrivateBin;
 
+use AppendIterator;
+use GlobIterator;
+
 /**
  * I18n
  *
@@ -193,10 +196,14 @@ class I18n
     public static function getAvailableLanguages()
     {
         if (count(self::$_availableLanguages) == 0) {
-            $i18n = dir(self::_getPath());
-            while (false !== ($file = $i18n->read())) {
-                if (preg_match('/^([a-z]{2,3}).json$/', $file, $match) === 1) {
-                    self::$_availableLanguages[] = $match[1];
+            self::$_availableLanguages[] = 'en'; // en.json is not part of the release archive
+            $languageIterator = new AppendIterator();
+            $languageIterator->append(new GlobIterator(self::_getPath('??.json')));
+            $languageIterator->append(new GlobIterator(self::_getPath('???.json'))); // for jbo
+            foreach ($languageIterator as $file) {
+                $language = $file->getBasename('.json');
+                if ($language != 'en') {
+                    self::$_availableLanguages[] = $language;
                 }
             }
         }