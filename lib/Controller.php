142a380bb2b7b65cc876a16a9828d455d08ebfe9
El RIDO <elrido@gmx.net>
2024-04-23 22:02:41 +0200

    undo simplification, here we actually check if it is a non-empty string

--- lib/Controller.php
+++ lib/Controller.php
@@ -114,7 +114,7 @@ class Controller
             error_log(I18n::_('%s requires php %s or above to work. Sorry.', I18n::_('PrivateBin'), self::MIN_PHP_VERSION));
             return;
         }
-        if (!defined('PATH') || substr(PATH, -1) !== DIRECTORY_SEPARATOR) {
+        if (strlen(PATH) < 0 && substr(PATH, -1) !== DIRECTORY_SEPARATOR) {
             error_log(I18n::_('%s requires the PATH to end in a "%s". Please update the PATH in your index.php.', I18n::_('PrivateBin'), DIRECTORY_SEPARATOR));
             return;
         }
