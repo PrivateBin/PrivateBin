5cc963be1b1a9799c0f51760a24ec867d17e7d3a
El RIDO <elrido@gmx.net>
2025-08-30 09:22:44 +0200

    deduplicate proxy view operations

--- lib/Controller.php
+++ lib/Controller.php
@@ -455,12 +455,7 @@ class Controller
         $page->assign('CSPHEADER', $metacspheader);
         $page->assign('ERROR', I18n::_($this->_error));
         $page->assign('NAME', $this->_conf->getKey('name'));
-        if ($this->_request->getOperation() === 'yourlsproxy') {
-            $page->assign('SHORTURL', $this->_status);
-            $page->draw('shortenerproxy');
-            return;
-        }
-        if ($this->_request->getOperation() === 'shlinkproxy') {
+        if (in_array($this->_request->getOperation(), array('shlinkproxy', 'yourlsproxy'), true)) {
             $page->assign('SHORTURL', $this->_status);
             $page->draw('shortenerproxy');
             return;
